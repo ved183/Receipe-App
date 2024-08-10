cf32dc3a8239c9487b387f5b12361cb11c2e3339
VEDANT TANDEL <vedanttandel188@gmail.com>
2024-08-10 01:22:14 -0700

    first commit

---
+++ receipe/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+ import './App.css';
+  import Mainpage from './Component/Mainpage';
+   import { Route,Routes } from 'react-router-dom'; 
+import Mealinfo from './Component/Mealinfo';
+  //import Mealinfo from './Component/Mealinfo'; 
+ // import Food from "./Components/Food" 
+  //import Recipe from './Components/Recipe'; 
+
+  function App() { 
+    return ( 
+      // <Mainpage/>
+       <Routes>
+         <Route path='/' element={<Mainpage/>}/>
+         <Route path='/:mealid' element={<Mealinfo/>}/>
+
+          </Routes>
+           );
+           } 
+           export default App;
